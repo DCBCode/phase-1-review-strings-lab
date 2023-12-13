4453ed230e14497eabe4c84b21866bb07a6c13ee
Drew Benson <Drewbenson16@gmail.com>
2023-12-13 00:13:25 -0700

    yeas

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code in this file!
+const currentUser = 'Drew Benson';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const shortGreeting = 'Welcome, ' + currentUser.charAt(0) + '!';
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, ";
+excitedWelcomeMessage += currentUser.toUpperCase();
+excitedWelcomeMessage += "!";
